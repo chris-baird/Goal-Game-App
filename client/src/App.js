f91b98ef23833640e2e74337763544c2748fd2b1
Chris Baird <chrisbaird05@icloud.com>
2020-09-15 17:02:36 -0700

    Intregrated react router

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -12,9 +13,15 @@ function App() {
             <li>
               <Link to="/">Home</Link>
             </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
           </ul>
         </nav>
         <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
           <Route path="/">
             <Home />
           </Route>
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const app = require("./app");
+const app = require('./app');
 
 // Starting server on given port
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server started");
+app.listen(process.env.PORT || 3001, () => {
+  console.log('Server started');
 });
