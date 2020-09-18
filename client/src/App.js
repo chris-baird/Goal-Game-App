cd1642d7a0c7c6a435d9a00d019c271ee62ab07c
Chris Baird <chrisbaird05@icloud.com>
2020-09-18 01:39:23 -0700

    Implemented Materal Ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
 import React, { useState } from "react";
-import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import GoalQuest from "./pages/GoalQuest";
 import WithAuth from "./components/WithAuth";
+import { Container, Typography, Link as UiLink } from "@material-ui/core/";
 
 function App() {
   const [user, setUser] = useState(null);
   return (
     <Router>
-      <div>
+      <Container>
         <nav>
           <ul>
             {/* Temp logout may refactor*/}
-            <li>{user ? <a href="/">Logout</a> : <Link to="/">Home</Link>}</li>
-            {!user && (
+            <Typography>
+              <UiLink href="#" onClick={4} variant="body2">
+                <li>
+                  {user ? <a href="/">Logout</a> : <Link to="/">Home</Link>}
+                </li>
+              </UiLink>
+              <UiLink href="#" onClick={4} variant="body2">
+                {!user && (
+                  <li>
+                    <Link to="/register">Register</Link>
+                  </li>
+                )}
+              </UiLink>
+              <UiLink href="#" onClick={4} variant="body2"></UiLink>
+            </Typography>
+            {/* <li>{user ? <a href="/">Logout</a> : <Link to="/">Home</Link>}</li> */}
+            {/* {!user && (
               <li>
                 <Link to="/register">Register</Link>
               </li>
-            )}
+            )} */}
           </ul>
         </nav>
         <Switch>
@@ -35,7 +50,7 @@ function App() {
             <Home user={user} setUser={setUser} />
           </Route>
         </Switch>
-      </div>
+      </Container>
     </Router>
   );
 }
